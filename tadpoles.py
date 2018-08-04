23d677987aff12732108f747d6acd26054654a3b
Allan Watkins <allan.watkins@endurance.com>
2018-08-04 11:19:18 -0500

    update directory and make filename hash of key

--- tadpoles.py
+++ tadpoles.py
@@ -1,10 +1,12 @@
 import time
 from requests import session
-from tpcredentials import *
+from tpcredentials import email, password, directory
+import hashlib
+import dropbox
 
 payload = { 'email': email, 'password': password }
 url = 'https://www.tadpoles.com/auth/login'
-
+filedir = directory
 
 def main():
     getimagelinks()
@@ -20,15 +22,28 @@ def getimagelinks():
                 result = c.get(link, stream=True)
                 if result.status_code == 200:
                     image = result.raw.read()
-                    now = time.time()
-                    filename = '/Users/awatkins/.virtualenvs/tadpoles/image_' + str(now) + '.jpg'
-                    open(filename, 'wb').write(image)
-                    print("writing link {} to file {}".format(imgkey,filename))
+                    filename = hashlib.md5(imgkey).hexdigest()
+                    open( filedir + filename + '.jpg', 'wb').write(image)
+                    print("writing image to file {}".format(imgkey,filename))
 
 
 def uploaddropbox():
-    return
 
+    client = dropbox.client.DropboxClient( < auth_token >)
+    print('linked account: ', client.account_info())
+
+    f = open('working-draft.txt', 'rb')
+    response = client.put_file('/magnum-opus.txt', f)
+    print('uploaded: ', response)
+
+    folder_metadata = client.metadata('/')
+    print('metadata: ', folder_metadata)
+
+    f, metadata = client.get_file_and_metadata('/magnum-opus.txt')
+    out = open('magnum-opus.txt', 'wb')
+    out.write(f.read())
+    out.close()
+    print(metadata)
 
 if __name__== "__main__":
   main()
