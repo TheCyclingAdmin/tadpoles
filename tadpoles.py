98b05a2951883ef69dbf958b38c66ecca3b1db43
Allan Watkins <allan.watkins@endurance.com>
2018-08-04 18:18:41 -0500

    added dropbox stuff

--- tadpoles.py
+++ tadpoles.py
@@ -1,14 +1,16 @@
 import time
 from requests import session
-from tpcredentials import email, password, directory
+from tpcredentials import email, password, directory, dbxtoken
 import hashlib
 import sys
 import os
 
 
 payload = { 'email': email, 'password': password }
-url = 'https://www.tadpoles.com/auth/login'
+baseurl = 'https://www.tadpoles.com/'
 path = directory
+dbx = dropbox.Dropbox(dbxtoken)
+
 
 def main():
     if not os.path.exists(path):
@@ -19,25 +21,130 @@ def main():
         sys.exit(1)
     else:
         downloadimgs()
+    for dn, dirs, files in os.walk(rootdir):
+        subfolder = dn[len(rootdir):].strip(os.path.sep)
+        listing = list_folder(dbx, folder, subfolder)
+        print('Descending into', subfolder, '...')
+
+        # First do all the files.
+        for name in files:
+            fullname = os.path.join(dn, name)
+            if not isinstance(name, six.text_type):
+                name = name.decode('utf-8')
+            nname = unicodedata.normalize('NFC', name)
+            if name.startswith('.'):
+                print('Skipping dot file:', name)
+            elif name.startswith('@') or name.endswith('~'):
+                print('Skipping temporary file:', name)
+            elif name.endswith('.pyc') or name.endswith('.pyo'):
+                print('Skipping generated file:', name)
+            elif nname in listing:
+                md = listing[nname]
+                mtime = os.path.getmtime(fullname)
+                mtime_dt = datetime.datetime(*time.gmtime(mtime)[:6])
+                size = os.path.getsize(fullname)
+                if (isinstance(md, dropbox.files.FileMetadata) and
+                        mtime_dt == md.client_modified and size == md.size):
+                    print(name, 'is already synced [stats match]')
+                else:
+                    print(name, 'exists with different stats, downloading')
+                    res = download(dbx, folder, subfolder, name)
+                    with open(fullname) as f:
+                        data = f.read()
+                    if res == data:
+                        print(name, 'is already synced [content match]')
+                    else:
+                        print(name, 'has changed since last sync')
+                        if yesno('Refresh %s' % name, False, args):
+                            upload(dbx, fullname, folder, subfolder, name,
+                                   overwrite=True)
+            elif yesno('Upload %s' % name, True, args):
+                upload(dbx, fullname, folder, subfolder, name)
 
 def downloadimgs():
     with session() as c:
         c.post(url, data=payload)
-        c.get('https://www.tadpoles.com/parents?app=parent&')
-        jsondata = c.get('https://www.tadpoles.com/remote/v1/events?direction=range&earliest_event_time=1&latest_event_time=99999999999&num_events=300&client=dashboard').json()
+        c.get(baseurl + 'parents?app=parent&')
+        jsondata = c.get(baseurl + 'remote/v1/events?direction=range&earliest_event_time=1&latest_event_time=99999999999&num_events=300&client=dashboard').json()
         for img in jsondata['events']:
             for imgkey in img['attachments']:
-                link = 'https://www.tadpoles.com/remote/v1/attachment?key=' + imgkey
+                link = baseurl + 'remote/v1/attachment?key=' + imgkey
                 result = c.get(link, stream=True)
                 if result.status_code == 200:
                     image = result.raw.read()
                     filename = hashlib.md5(imgkey.encode('utf-8')).hexdigest() + '.jpg'
-                    open(path + filename, 'wb').write(image)
-                    print("writing image to file {}".format(imgkey,filename))
+                    if not isfile(filename):
+                        open(path + filename, 'wb').write(image)
+                        print("writing image to file {}".format(imgkey,filename))
 
 
-def uploaddropbox():
-    return
+def list_folder(dbx, folder, subfolder):
+    """List a folder.
+    Return a dict mapping unicode filenames to
+    FileMetadata|FolderMetadata entries.
+    """
+    path = '/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'))
+    while '//' in path:
+        path = path.replace('//', '/')
+    path = path.rstrip('/')
+    try:
+        with stopwatch('list_folder'):
+            res = dbx.files_list_folder(path)
+    except dropbox.exceptions.ApiError as err:
+        print('Folder listing failed for', path, '-- assumed empty:', err)
+        return {}
+    else:
+        rv = {}
+        for entry in res.entries:
+            rv[entry.name] = entry
+        return rv
+
+
+def download(dbx, folder, subfolder, name):
+    """Download a file.
+
+    Return the bytes of the file, or None if it doesn't exist.
+    """
+    path = '/%s/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'), name)
+    while '//' in path:
+        path = path.replace('//', '/')
+    with stopwatch('download'):
+        try:
+            md, res = dbx.files_download(path)
+        except dropbox.exceptions.HttpError as err:
+            print('*** HTTP error', err)
+            return None
+    data = res.content
+    print(len(data), 'bytes; md:', md)
+    return data
+
+
+def upload(dbx, fullname, folder, subfolder, name, overwrite=False):
+    """Upload a file.
+
+    Return the request response, or None in case of error.
+    """
+    path = '/%s/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'), name)
+    while '//' in path:
+        path = path.replace('//', '/')
+    mode = (dropbox.files.WriteMode.overwrite
+            if overwrite
+            else dropbox.files.WriteMode.add)
+    mtime = os.path.getmtime(fullname)
+    with open(fullname, 'rb') as f:
+        data = f.read()
+    with stopwatch('upload %d bytes' % len(data)):
+        try:
+            res = dbx.files_upload(
+                data, path, mode,
+                client_modified=datetime.datetime(*time.gmtime(mtime)[:6]),
+                mute=True)
+        except dropbox.exceptions.ApiError as err:
+            print('*** API error', err)
+            return None
+    print('uploaded as', res.name.encode('utf8'))
+    return res
+
 
 if __name__== "__main__":
   main()
