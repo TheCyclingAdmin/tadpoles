9f23c277be8706e1ac90f66824e85221715525e4
TheCyclingAdmin <allan@thecyclingadmin.com>
2018-09-10 11:03:49 -0500

    commentary

--- tpcredentials.example.py
+++ tpcredentials.example.py
@@ -1,11 +1,24 @@
+#tadpoles login details
 email = 'tadpoleslogin'
 password = 'tadpolespassword'
+
+#folder on local computer / server
 localdir = '/somefolder/'
+
+#dropbox app api token
 dbxtoken = 'dropboxtoken'
+#dropbox folder
 dbxfolder = '/dropboxfolder'
-gmailuser = 'ilikeusing@gmail.com'
-gmailpwd = 'gmailapppassword'
-recipients = ['one@email.com', 'two@email.com', 'more@email.com']
+#shareable drop box links (so that emails include the links)
 dropfolder = 'https://www.dropbox.com/longlink'
 droppreview = 'https://www.dropbox.com/home/foldername?preview='
+
+#gmail info for sending address
+gmailuser = 'ilikeusing@gmail.com'
+gmailpwd = 'gmailapppassword'
+
+#list of recipient emails
+recipients = ['one@email.com', 'two@email.com', 'more@email.com']
+
+#name of kiddo (or whatever you want to add to subject line)
 kidsname = 'kidsname for subject line'
